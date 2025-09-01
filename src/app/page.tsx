edf7015d7cca47602b41ddeb8fe6f1fbc94f0649
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-09-01 12:04:26 +0300

    added about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import About from "@/components/homePage/about/About";
 import MarqueeLine from "@/components/shared/marquee/MarqueeLine";
 
 export default function HomePage() {
   return (
     <>
       <MarqueeLine />
+      <About />
     </>
   );
 }
---
+++ src/components/homePage/about/BenefitsList.tsx
@@ -0,0 +1,26 @@
+import * as motion from "motion/react-client";
+import { listVariants } from "@/utils/animationVariants";
+import BenefitItem from "./BenefitItem";
+
+export default function BenefitsList() {
+  const benefitsList = [
+    { value: "10+", description: "років досвіду" },
+    { value: "100%", description: "сертифіковані фахівці" },
+    { value: "1000+", description: "задоволених відвідувачів" },
+  ];
+
+  return (
+    <motion.ul
+      initial="hidden"
+      whileInView="visible"
+      exit="exit"
+      viewport={{ once: true, amount: 0.4 }}
+      variants={listVariants({ staggerChildren: 0.3, delayChildren: 0.3 })}
+      className="flex flex-col sm:flex-row gap-3.5 xl:gap-5"
+    >
+      {benefitsList.map((benefit, idx) => (
+        <BenefitItem key={idx} benefit={benefit} idx={idx} />
+      ))}
+    </motion.ul>
+  );
+}
