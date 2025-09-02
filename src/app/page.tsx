17407eddcfd50581a1da88cc6a6c369635d536a2
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-09-02 11:14:13 +0300

    added home page cta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import About from "@/components/homePage/about/About";
 import Blog from "@/components/homePage/blog/Blog";
 import ChooseUs from "@/components/homePage/chooseUs/ChooseUs";
 import Mission from "@/components/homePage/mission/Mission";
+import Reviews from "@/components/homePage/reviews/Reviews";
+import StartJourneyCTA from "@/components/shared/cta/StartJourneyCTA";
 import MarqueeLine from "@/components/shared/marquee/MarqueeLine";
 
 export default function HomePage() {
@@ -13,6 +15,11 @@ export default function HomePage() {
       <Mission />
       <Blog />
       <MarqueeLine className="hidden lg:block lg:text-[26px] lg:h-[65px]" />
+      <Reviews />
+      <StartJourneyCTA
+        image="/images/homePage/cta/office.webp"
+        className="py-15"
+      />
     </>
   );
 }
