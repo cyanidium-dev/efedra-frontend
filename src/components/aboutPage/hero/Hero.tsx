31e84f84401cca1ccbc8dcf3af0bc37c079c970e
Sofia-Madryha <sonyamadryha3@gmail.com>
2025-09-03 15:52:32 +0100

    add priority to image

--- src/components/aboutPage/hero/Hero.tsx
+++ src/components/aboutPage/hero/Hero.tsx
@@ -64,6 +64,7 @@ export default function Hero() {
               alt="Aesthetic medicine center"
               className="object-cover object-center"
               fill
+              priority
             />
           </motion.div>
         </div>
