ca886fc6211e44c3e2a3bf527b5aa92cf56b0eb9
Sofia-Madryha <sonyamadryha3@gmail.com>
2025-09-04 16:25:59 +0100

    fix styles

--- src/components/aboutPage/advantages/Advantages.tsx
+++ src/components/aboutPage/advantages/Advantages.tsx
@@ -42,7 +42,7 @@ export default function Advantages() {
     <section className="pt-[60px] lg:pt-[72px]">
       <Container>
         <SectionTitle>Наші переваги</SectionTitle>
-        <div className="flex flex-col gap-6 pt-1 md:hidden">
+        <div className="flex flex-col gap-6 pt-1 justify-center items-center md:hidden">
           <motion.ul
             initial="hidden"
             whileInView="visible"
@@ -52,7 +52,7 @@ export default function Advantages() {
               staggerChildren: 0.3,
               delayChildren: 0.3,
             })}
-            className="flex flex-col gap-6"
+            className="flex flex-col sm:flex-row justify-center items-center gap-6 sm:gap-4 w-full"
           >
             {advantages.slice(0, 2).map((item, i) => (
               <AdvantageItem key={i} {...item} />
@@ -65,7 +65,7 @@ export default function Advantages() {
             exit="exit"
             viewport={{ once: true, amount: 0.3 }}
             variants={fadeInAnimation({ scale: 0.95, delay: 0.3 })}
-            className="relative w-full md:w-[calc(50%)] h-[319px]  sm:h-[390px] overflow-hidden rounded-tl-[300px] rounded-tr-[300px] rounded-br-[20px] rounded-bl-[300px]"
+            className="relative w-full h-[319px] min-[480px]:max-w-[400px] sm:max-w-full sm:h-[390px] overflow-hidden rounded-tl-[300px] rounded-tr-[300px] rounded-br-[20px] rounded-bl-[300px]"
           >
             <Image
               src="/images/aboutPage/advantages/destistry.webp"
@@ -84,7 +84,7 @@ export default function Advantages() {
               staggerChildren: 0.3,
               delayChildren: 0.3,
             })}
-            className="flex flex-col gap-6"
+            className="flex flex-col sm:flex-row justify-center items-center gap-6 sm:gap-4 w-full"
           >
             {advantages.slice(2, 4).map((item, i) => (
               <AdvantageItem key={i} {...item} />
@@ -92,13 +92,13 @@ export default function Advantages() {
           </motion.ul>
         </div>
 
-        <div className="hidden md:flex items-center pt-[31.17px]">
+        <div className="hidden md:flex  items-center pt-[31.17px]">
           <motion.ul
             initial="hidden"
             whileInView="visible"
             exit="exit"
             viewport={{ once: true, amount: 0.4 }}
-            className="flex flex-col gap-7 lg:gap-0 md:w-[calc(50%-10px)] lg:w-[calc(50%)]"
+            className="flex flex-col  gap-7 lg:gap-0 md:w-[calc(50%+50px)] lg:w-[calc(50%)]"
           >
             {advantages.map((item, i) => (
               <AdvantageItem key={i} idx={i} {...item} />
@@ -110,7 +110,7 @@ export default function Advantages() {
             exit="exit"
             viewport={{ once: true, amount: 0.3 }}
             variants={fadeInAnimation({ scale: 0.95, delay: 0.3 })}
-            className="relative w-full md:w-[calc(50%+10px)] lg:w-[calc(50%+10px)] xl:w-[calc(50%)]  md:h-[600px] lg:h-[625px] xl:h-[625px] overflow-hidden rounded-tl-[300px] rounded-tr-[300px] rounded-br-[20px] rounded-bl-[300px]"
+            className="relative w-full md:w-[calc(50%-50px)] lg:w-[calc(50%-5px)] xl:w-[calc(50%)]  md:h-[295px] lg:h-[420px] xl:h-[625px] overflow-hidden  rounded-tl-[300px] rounded-tr-[300px] rounded-br-[20px] rounded-bl-[300px]"
           >
             <Image
               src="/images/aboutPage/advantages/destistry.webp"
