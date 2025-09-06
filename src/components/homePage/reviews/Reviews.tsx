6db2e74a61964947d402bbb4ee1fbd69073d4e4f
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-09-06 11:00:03 +0300

    added rate us

--- src/components/homePage/reviews/Reviews.tsx
+++ src/components/homePage/reviews/Reviews.tsx
@@ -1,15 +1,26 @@
 import Container from "@/components/shared/container/Container";
 import SectionTitle from "@/components/shared/titles/SectionTitle";
+import * as motion from "motion/react-client";
+import { fadeInAnimation } from "@/utils/animationVariants";
+import RateUs from "./RateUs";
 
 export default function Reviews() {
   return (
     <section id="reviews" className="pt-15 lg:pt-16 lg:pb-[115px] scroll-mt-11">
       <Container>
         <SectionTitle>Відгуки наших клієнтів</SectionTitle>
-        <p className="max-w-[544px] font-evolenta text-[24px] lg:text-[32px] font-normal leading-[133%] uppercase text-blue">
+        <motion.p
+          initial="hidden"
+          whileInView="visible"
+          exit="exit"
+          viewport={{ once: true, amount: 0.3 }}
+          variants={fadeInAnimation({ x: 30 })}
+          className="max-w-[544px] mb-10 lg:mb-3 font-evolenta text-[24px] lg:text-[32px] font-normal leading-[133%] uppercase text-blue"
+        >
           Вони вже довірили нам свою красу - тепер{" "}
           <span className="italic">ваша черга</span>!
-        </p>
+        </motion.p>
+        <RateUs />
       </Container>
     </section>
   );
