8fa82873595e7a4abd2c9a78c660c9e74125c4a6
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-09-09 14:48:53 +0300

    added hero images

--- src/components/blogPage/hero/Hero.tsx
+++ src/components/blogPage/hero/Hero.tsx
@@ -1,11 +1,35 @@
 import Container from "@/components/shared/container/Container";
+import Image from "next/image";
 
 export default function Hero() {
   return (
     <section className="pt-3.5 pb-12 lg:pt-[42px] lg:pb-[105px]">
       <Container className="flex flex-col gap-[26px]">
-        <div className="relative rounded-[20px] overflow-hidden"></div>
-        <div></div>
+        <div className="relative w-full h-[199px] rounded-[20px] overflow-hidden">
+          <Image
+            src="/images/blogPage/hero/imageOne.webp"
+            alt="background"
+            fill
+            priority
+            className="object-cover"
+          />
+        </div>
+        <div>
+          <div className="flex gap-6">
+            <h1 className="font-evolenta text-[24px] font-normal leading-[133%] uppercase">
+              Блог Efedra: експертно й доступно
+            </h1>
+            <div className="relative w-[115px] h-auto rounded-[20px] overflow-hidden shrink-0">
+              <Image
+                src="/images/blogPage/hero/imageTwo.webp"
+                alt="background"
+                fill
+                priority
+                className="object-cover"
+              />
+            </div>
+          </div>
+        </div>
       </Container>
     </section>
   );
