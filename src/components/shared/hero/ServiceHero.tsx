f598bc32996f8e79148f21afb623262458fe1a17
Sofia-Madryha <sonyamadryha3@gmail.com>
2025-09-12 18:31:55 +0100

    added animation

--- src/components/shared/hero/ServiceHero.tsx
+++ src/components/shared/hero/ServiceHero.tsx
@@ -24,21 +24,21 @@ export default function ServiceHero({ title, image }: ServiceHeroProps) {
           whileInView="visible"
           exit="exit"
           viewport={{ once: true, amount: 0.3 }}
-          variants={fadeInAnimation({ scale: 0.95, x: 50, duration: 1.2 })}
+          variants={fadeInAnimation({ x: 30 })}
           className="max-h-[195px] lg:max-h-[380px] text-[16px] md:text-[28px] lg:text-[32px] leading-[115%] lg:leading-[135%] font-evolenta uppercase self-end [writing-mode:sideways-lr]"
         >
           {title}
         </motion.h1>
 
-        <motion.div
-          initial="hidden"
-          whileInView="visible"
-          exit="exit"
-          viewport={{ once: true, amount: 0.3 }}
-          variants={fadeInAnimation({ scale: 0.95, x: 50, duration: 1.2 })}
-          className="relative w-full"
-        >
-          <div className="relative w-full h-[195px] md:h-[320px] lg:h-[380px] xl:h-[472px] rounded-[20px] overflow-hidden">
+        <div className="relative w-full">
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            exit="exit"
+            viewport={{ once: true, amount: 0.3 }}
+            variants={fadeInAnimation({ x: 20 })}
+            className="relative w-full h-[195px] md:h-[320px] lg:h-[380px] xl:h-[472px] rounded-[20px] overflow-hidden"
+          >
             <Image
               src={image}
               alt="background"
@@ -46,26 +46,34 @@ export default function ServiceHero({ title, image }: ServiceHeroProps) {
               priority
               className="object-cover"
             />
-          </div>
-          <MainButton
-            variant="bordered"
-            withArrow
-            className="md:hidden absolute -bottom-[20px] -right-[12px] px-[17px] max-w-[158px] h-[43px] text-[14px]"
-            iconClassName="w-[26px] h-[21px]"
+          </motion.div>
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            exit="exit"
+            viewport={{ once: true, amount: 0.3 }}
+            variants={fadeInAnimation({ x: 50, delay: 0.3 })}
           >
-            Записатися
-          </MainButton>
+            <MainButton
+              variant="bordered"
+              withArrow
+              className="md:hidden absolute -bottom-[20px] -right-[12px] px-[17px] max-w-[158px] h-[43px] text-[14px]"
+              iconClassName="w-[26px] h-[21px]"
+            >
+              Записатися
+            </MainButton>
 
-          <MainButton
-            onClick={() => setIsModalShown(true)}
-            variant="white"
-            withArrow
-            className="hidden md:flex absolute bottom-[16px] right-[15px] px-[23px] lg:px-[23px] max-w-[175px] h-[56px] text-[15px] leading-[120%] text-black"
-            iconClassName="w-[26px] h-[21px]"
-          >
-            Записатися
-          </MainButton>
-        </motion.div>
+            <MainButton
+              onClick={() => setIsModalShown(true)}
+              variant="white"
+              withArrow
+              className="hidden md:flex absolute bottom-[16px] right-[15px] px-[23px] lg:px-[23px] max-w-[175px] h-[56px] text-[15px] leading-[120%] text-black"
+              iconClassName="w-[26px] h-[21px]"
+            >
+              Записатися
+            </MainButton>
+          </motion.div>
+        </div>
       </Container>
       <CallBackModal
         isModalShown={isModalShown}
