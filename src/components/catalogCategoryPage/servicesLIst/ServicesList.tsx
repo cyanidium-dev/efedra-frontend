227629969020ce2c8fb61729a1bb7fd9be6b89da
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-09-13 19:56:23 +0300

    added hero section

---
+++ src/components/catalogCategoryPage/servicesLIst/ServicesList.tsx
@@ -0,0 +1,9 @@
+import Container from "@/components/shared/container/Container";
+
+export default function ServicesList() {
+  return (
+    <section className="pt-8 lg:pt-25">
+      <Container></Container>
+    </section>
+  );
+}
