600505fbdd543fe20c1e0c526edcd475dc9493e8
Sofia-Madryha <sonyamadryha3@gmail.com>
2025-09-16 12:14:19 +0100

    fix mobile table styles

--- src/components/pricePage/PriceList/ServicePriceList.tsx
+++ src/components/pricePage/PriceList/ServicePriceList.tsx
@@ -13,7 +13,7 @@ export default function ServicePriceList({
   return (
     <>
       <div
-        className={`hidden md:grid md:grid-cols-[2.5fr_1.75fr_0.7fr] lg:grid-cols-[2.5fr_1.75fr_0.5fr] border-x rounded-b-[32px] border-b border-t border-${colorScheme} rounded-[32px] mt-[6px]`}
+        className={`hidden md:grid md:grid-cols-[2.5fr_1.75fr_0.7fr] lg:grid-cols-[2.5fr_1.75fr_0.5fr] border-x border-b border-t border-${colorScheme} rounded-[32px] mt-[6px]`}
       >
         <div className="pl-[46px] py-[18px] uppercase font-evolenta text-[12px] md:text-[14px] text-left">
           Назва послуги
@@ -58,17 +58,16 @@ export default function ServicePriceList({
           })}
       </div>
 
-      <div className="md:hidden">
+      <div
+        className={`md:hidden mt-[6px] border border-${colorScheme} rounded-[32px] overflow-hidden`}
+      >
         {services
           .filter((service) => service?.title)
-          .map((service, idx, arr) => {
-            const isLast = idx === arr.length - 1;
+          .map((service, idx) => {
             return (
               <div
                 key={idx}
-                className={`flex flex-col gap-2 px-3 py-4 text-sm border-t-[0.5px] border-x border-${colorScheme} ${
-                  isLast ? "border-b rounded-b-[32px]" : ""
-                }`}
+                className={`flex flex-col gap-2 px-3 py-4 text-sm border-t-[0.5px] border-${colorScheme} ${idx === 0 ? "border-t-[0px]" : ""}`}
               >
                 <p className="text-[12px] leading-[133%]">{service.title}</p>
                 <div className="flex justify-between text-[12px] leading-[133%]">
